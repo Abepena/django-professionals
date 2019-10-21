564ee9fc9f09491739d72f98f09408a2f5a30075
Abe Pena <pena.abe@gmail.com>
2019-10-21 19:35:13 -0400

    ch 3

---
+++ books/users/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model # looks for AUTH_USER_MODEL in settings
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta: 
+        model = get_user_model()
+        fields = ('email', 'username',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email','username',)
